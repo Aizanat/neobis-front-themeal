b619abf1b1fa8fe705150dfcdd1ed923b5be94fb
Aizanat <anukieva@mail.ru>
2024-04-04 17:06:55 +0400

    created a detailed dish page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
 import MealDay from './components/MealDay'
+import MealDetails from './components/MealDetails'
 
 function App() {
   return (
     <div className="App">
-      <MealDay />
+      <MealDetails />
     </div>
   )
 }

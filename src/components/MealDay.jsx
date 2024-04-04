f878886e207b3bafaf063e0f4c3c8f42c5846d61
Aizanat <anukieva@mail.ru>
2024-04-04 21:11:26 +0400

    wrote and connected requests for obtaining a random dish

--- src/components/MealDay.jsx
+++ src/components/MealDay.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/mealDay.css'
 import Header from './Header'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const MealDay = () => {
+  const [result, setResult] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://www.themealdb.com/api/json/v1/1/random.php'
+        )
+        setResult(response.data.meals[0])
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
       <Header />
@@ -10,11 +28,15 @@ const MealDay = () => {
         <div className="container">
           <div className="mealDay__info">
             <h2 className="mealDay__text">Meal of the Day</h2>
-            <a href="#">Smoked Haddock Kedgeree</a>
-            <p className="mealDay__category">Breakfast | Indian</p>
+            <Link to={`/details/${result.idMeal}`} className="mealDay__name">
+              {result.strMeal}
+            </Link>
+            <p className="mealDay__category">
+              {result.strArea} <span>{result.strCategory}</span>
+            </p>
           </div>
           <div className="mealDay__image">
-            <img src="" alt="mealImage" />
+            <img src={result.strMealThumb} alt="mealImage" />
           </div>
           <div className="search__block">
             <h2 className="search__text">Find your Meal</h2>

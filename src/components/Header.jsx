6ecc45da749c0d8429aef358a9ad7cadbb47e5a6
Aizanat <anukieva@mail.ru>
2024-04-04 14:54:01 +0400

    made a profile header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="/"> The Meal</a>
+    </div>
+  )
+}
+
+export default Header
